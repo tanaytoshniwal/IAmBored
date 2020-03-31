48899b49729f303de75b1c4de7a33b0b459dd105
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2020-03-31 13:02:21 +0530

    init meme

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Container from 'react-native-container';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import { Button } from 'native-base';
 
 class App extends React.Component {
   render() {
     return (
       <Container style={styles.container}>
+        <View style={styles.imageView}>
+          <Image style={styles.image} source={{ uri: 'https://i.pinimg.com/474x/bf/28/45/bf28450fec051c9c1d57ce7eedb8e422.jpg' }} />
+        </View>
         <Button style={styles.button}>
           <Text style={styles.btnText}>Get another meme!</Text>
         </Button>
@@ -31,8 +34,17 @@ const styles = StyleSheet.create({
     elevation: 9
   },
   btnText: {
-    fontWeight: 'bold',
-    fontFamily: 'Muli'
+    fontFamily: 'Muli',
+    fontSize: 15,
+    fontWeight: '600'
+  },
+  image: {
+    width: 360,
+    height: 360,
+    resizeMode: 'contain'
+  },
+  imageView: {
+    marginBottom: 25
   }
 })
 
